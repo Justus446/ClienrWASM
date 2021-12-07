a5b6cb12ecaa260071b2ba518ae2587b175c587e
Justus Omondi <85983194+Justus446@users.noreply.github.com>
2021-12-07 08:42:54 +0300

    Add project files.

---
+++ ClienrWASM/Shared/ProdInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienrWASM.Shared
+{
+    public class ProdInfo
+    {
+        public int ProductID { get; set; }
+        public string SKU { get; set; }
+
+        public string Description { get; set; }
+        public int WareID { get; set; }
+        public string Warehouse { get; set; }
+        public string Location { get; set; }
+
+        public decimal InStock { get; set; }
+    }
+}
